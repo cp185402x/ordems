dcf0cc78c6bb68b496a00f0a9f571b626fa9d2c6
vinicius lazarini pecchia <viniciuslazarinipecchia@gmail.com>
2019-09-11 22:28:33 -0300

    Update de novos packs

---
+++ ordems/src/Interface peca/com/view/fornecedor/gui/CreateFornecedor.java
@@ -0,0 +1,12 @@
+
+package com.view.fornecedor.gui;
+
+import javax.swing.JFrame;
+
+public class CreateFornecedor {
+    
+    public static void main(String[] args) {
+    	FornecedorView FornecedorView = new FornecedorView ();
+        
+    	    }
+}
---
+++ ordems/src/peca/view/CreatePeca.java
@@ -0,0 +1,12 @@
+
+package peca.view;
+
+import javax.swing.JFrame;
+
+public class CreatePeca {
+    
+    public static void main(String[] args) {
+    	PecaView PecaView = new PecaView ();
+        
+    	    }
+}
